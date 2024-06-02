3e91ff5a7e566b7b32d00a9be7de3ec61dc10a4d
bobronaud <bobronaud@gmail.com>
2024-06-02 21:18:49 +0300

    add ESLint

---
+++ client/eslint.config.mjs
@@ -0,0 +1,19 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
+
+const customRules = {
+  'react/react-in-jsx-scope': 'off',
+};
+
+export default [
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ...pluginReactConfig,
+    settings: { ...pluginReactConfig.settings, react: { version: 'detect' } },
+  },
+  { rules: customRules },
+];
