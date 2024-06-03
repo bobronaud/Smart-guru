595fe43d32cf7ddf76277e1cc6b3d4464f9cd5fb
bobronaud <bobronaud@gmail.com>
2024-06-03 20:26:22 +0300

    add husky

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 const App = () => {
 	const [count, setCount] = useState(0);
 	const handleclick = () => {
-		setCount((prev) => prev + 1);
+		setCount(prev => prev + 1);
 	};
 	return (
 		<div>
