aa197201a0ee5f4006253ba0ce66e4ef105cdc12
bobronaud <bobronaud@gmail.com>
2024-06-16 10:44:53 +0300

    create CardCourse component

--- src/pages/uiKit/UiKit.tsx
+++ src/pages/uiKit/UiKit.tsx
@@ -1,5 +1,6 @@
 import Clock from '@/assets/icons/clock.svg';
 import Button from '@/components/ui/button/Button';
+import Place from '@/components/ui/place/Place';
 import styles from './UiKit.module.scss';
 
 const UiKit = () => {
@@ -99,6 +100,14 @@ const UiKit = () => {
 						<p>Кнопка</p>
 						<Clock />
 					</Button>
+				</div>
+			</div>
+			<hr />
+			<div /*div с лейблами о наборе*/>
+				<div className={styles.block}>
+					<Place type={'inProgress'} />
+					<Place type={'almostFinished'} />
+					<Place type={'finished'} />
 				</div>
 			</div>
 			<hr />
