026bf344949765940ac95f62c8834ea2c3120f68
Ilkaon <ilnurnasyrov35@gmail.com>
2024-06-25 20:25:53 +0300

    Correct header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,7 @@
-import { useState } from 'react';
 import classes from './Header.module.scss';
 import TopBar from './TopBar/TopBar';
 const Header = () => {
-	const [advertisement, setAdvertisement] = useState(false);
-	const handleclick = () => {
-		setAdvertisement(!advertisement);
-	};
+	const advertisement = false;
 	return (
 		<header className={classes.mainContainer}>
 			<TopBar />
@@ -13,17 +9,14 @@ const Header = () => {
 				<div className={classes.captionBox}>
 					<h1 className={classes.caption}>Каталог</h1>
 				</div>
-				<div className={classes.bannerBox}>
-					{advertisement ? (
+				{advertisement && (
+					<div className={classes.bannerBox}>
 						<img
-							onClick={handleclick}
 							className={classes.banner}
 							src='https://avatars.dzeninfra.ru/get-zen_doc/3940836/pub_621b82a616b4077ea9c412e4_621b8373cddbf118aab0ae31/scale_1200'
 						/>
-					) : (
-						<button onClick={handleclick}>Показать баннер</button>
-					)}
-				</div>
+					</div>
+				)}
 			</section>
 		</header>
 	);
