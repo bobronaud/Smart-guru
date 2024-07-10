939c5462288aba9933e3efa7a11ceec8715d19ee
bobronaud <bobronaud@gmail.com>
2024-07-10 19:55:54 +0300

    add role modal

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,15 @@ import { openModal } from './store/slices/modalSlice';
 const App = () => {
 	const dispatch = useAppDispatch();
 	const handleclick = () => {
-		dispatch(openModal('recoveryNewPassword'));
+		dispatch(openModal('role'));
+	};
+	const handleclick2 = () => {
+		dispatch(openModal('logIn'));
 	};
 	return (
 		<div>
 			<Button onClick={handleclick}>open modal</Button>
+			<Button onClick={handleclick2}>open modal</Button>
 		</div>
 	);
 };
--- src/components/modal/role/Role.tsx
+++ src/components/modal/role/Role.tsx
@@ -1,7 +1,47 @@
+import CloseSVG from '@/assets/icons/close.svg';
+import imgRoleOrganization from '@/assets/images/roleOrganization.png';
+import imgRoleUser from '@/assets/images/roleUser.png';
+import Button from '@/components/ui/button/Button';
+import { useAppDispatch } from '@/hooks';
+import { closeModal } from '@/store/slices/modalSlice';
 import styles from './Role.module.scss';
 
 const Role = () => {
-	return <div className={styles.container}>Выбор роли</div>;
+	const dispatch = useAppDispatch();
+	const handleClose = () => {
+		dispatch(closeModal());
+	};
+
+	return (
+		<div className={styles.container}>
+			<div className={styles.header}>
+				<h2 className={styles.title}>Выберите роль</h2>
+				<CloseSVG onClick={handleClose} />
+			</div>
+			<div className={styles.roles}>
+				<div className={styles.role}>
+					<h4 className={styles.title}>Пользователь</h4>
+					<img
+						src={imgRoleUser}
+						alt='Изображение роли пользователя'
+					/>
+				</div>
+				<div className={styles.role}>
+					<h4 className={styles.title}>Организация</h4>
+					<img
+						src={imgRoleOrganization}
+						alt='Изображение роли организации'
+					/>
+				</div>
+			</div>
+			<Button
+				onClick={handleClose}
+				className={styles.button}
+			>
+				Продолжить
+			</Button>
+		</div>
+	);
 };
 
 export default Role;
