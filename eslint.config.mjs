43f98b6b8e4ad6ea4caf3af358bcad776dd3a2ff
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-07-11 23:02:54 +0300

    fix: fix type props in config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,13 @@
-import globals from 'globals';
 import pluginJs from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
 import eslintConfigPrettier from 'eslint-config-prettier';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 const customRules = {
 	'react/react-in-jsx-scope': 'off',
+	'react/prop-types': 'off',
 };
 
 const ignores = ['**/node_modules/', '**/public/', '**/assets/', '**/build/'];
