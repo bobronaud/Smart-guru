5beb5e56079433c756e5b28ff843279187410d7d
Aleksey Grishkin <kvgyzqk1996@yandex.ru>
2024-07-17 14:48:32 +0300

    доделана страница с настройками профиля и уведомлений

--- src/components/mySettings/MySettings.tsx
+++ src/components/mySettings/MySettings.tsx
@@ -1,5 +1,6 @@
 import { FC, useState } from 'react';
 import styles from './MySettings.module.scss';
+import { NotificationsSettings } from './notificators/NotificationsSettings';
 import { ProfileSettings } from './profile/ProfileSettings';
 import Avatar from './Rectangle 119.png';
 
@@ -33,13 +34,27 @@ export const MySettingsComponent: FC = () => {
 					Уведомления
 				</button>
 			</div>
-			<div id='profile'>
+			<div
+				id='profile'
+				className={`${styles.tab_content} ${activeTab === 'profile' ? styles.active : ''}`}
+			>
 				<ProfileSettings
 					imageSrc={userData.imageSrc}
 					login={userData.login}
 					email={userData.email}
 					password={userData.password}
 				/>
+			</div>
+			<div
+				id='notifications'
+				className={`${styles.tab_content} ${activeTab === 'notifications' ? styles.active : ''}`}
+			>
+				<div className={styles.switch_tab_content}>
+					<NotificationsSettings label='Отправлять на e-mail' />
+					<NotificationsSettings label='Уведомления о записанных курсах' />
+					<NotificationsSettings label='Уведомления о получении сертификата' />
+					<NotificationsSettings label='Еще что-то' />
+				</div>
 			</div>
 		</div>
 	);
